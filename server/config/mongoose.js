a492ad8a67fe7ed8dbad95edd7fa55d89f834c13
David Chen <chentiki808@gmail.com>
2016-04-03 17:50:50 -0700

    initial commit

---
+++ server/config/mongoose.js
@@ -0,0 +1,21 @@
+// MONGOOSE JS
+
+// require MONGOOSE
+var mongoose = require('mongoose');
+
+// require file-system (to load, read, require model file)
+var fs = require('fs');
+
+// connecting to database
+mongoose.connect('mongodb://localhost/mean_boilerplate');
+
+
+// specify the path to all the models
+var models_path = __dirname + '/../models'
+
+// read all of the files in the models_path
+fs.readdirSync(models_path).forEach(function(file){
+	if(file.indexOf('js') > 0){
+		require(models_path + '/' + file);
+	}
+})
---
+++ server/config/routes.js
@@ -0,0 +1,19 @@
+// ROUTES JS
+
+// require tweets controller
+//var tweets = require('./../controllers');
+
+
+
+module.exports = function(app){
+
+	// GETS ==================================
+	
+
+
+	// POSTS =================================
+
+
+
+
+}
